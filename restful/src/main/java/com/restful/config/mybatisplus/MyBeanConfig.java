391a53f51b4499a49c1c50b85740a70bc8997824
李德健 <git@192.168.2.135:web/gwkj-base.git>
2018-08-15 11:41:46 +0800

    创建用户的时候 将密码加密， 可以直接使用用户登录

---
+++ restful/src/main/java/com/restful/config/mybatisplus/MyBeanConfig.java
@@ -0,0 +1,39 @@
+package com.restful.config.mybatisplus;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ *
+ *  前端控制器
+ *
+ * @author baily
+ * 描述:
+ * ${DESCRIPTION}
+ *   ┏ ┓   ┏ ┓
+ *  ┏┛ ┻━━━┛ ┻┓
+ *  ┃         ┃
+ *  ┃    ━    ┃
+ *  ┃  ┳┛  ┗┳ ┃
+ *  ┃         ┃
+ *  ┃    ┻    ┃
+ *  ┃         ┃
+ *  ┗━━┓    ┏━┛
+ *     ┃    ┃神兽保佑
+ *     ┃    ┃代码无BUG！
+ *     ┃    ┗━━━━━━━┓
+ *     ┃            ┣┓
+ *     ┃            ┏┛
+ *     ┗┓┓┏━━━━━━┳┓┏┛
+ *      ┃┫┫      ┃┫┫
+ *      ┗┻┛      ┗┻┛
+ * @since 2018年08月15日
+ */
+@Configuration
+public class MyBeanConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
