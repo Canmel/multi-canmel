d2252f1fb1cd74370ab528b634a41b7f4d4ef858
李德健 <git@192.168.2.135:web/gwkj-base.git>
2018-09-10 00:23:55 +0800

    初步使用activiti

---
+++ restful/src/main/java/com/restful/config/activiti/ActitytiDataSourceConfig.java
@@ -0,0 +1,62 @@
+package com.restful.config.activiti;
+
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.activiti.spring.boot.AbstractProcessEngineAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ *  前端控制器
+ *
+ * @author baily
+ * 描述:
+ * ${DESCRIPTION}
+ *   ┏ ┓   ┏ ┓
+ *  ┏┛ ┻━━━┛ ┻┓
+ *  ┃         ┃
+ *  ┃    ━    ┃
+ *  ┃  ┳┛  ┗┳ ┃
+ *  ┃         ┃
+ *  ┃    ┻    ┃
+ *  ┃         ┃
+ *  ┗━━┓    ┏━┛
+ *     ┃    ┃神兽保佑
+ *     ┃    ┃代码无BUG！
+ *     ┃    ┗━━━━━━━┓
+ *     ┃            ┣┓
+ *     ┃            ┏┛
+ *     ┗┓┓┏━━━━━━┳┓┏┛
+ *      ┃┫┫      ┃┫┫
+ *      ┗┻┛      ┗┻┛
+ * @since 2018年09月08日
+ */
+
+//@Configuration
+public class ActitytiDataSourceConfig extends AbstractProcessEngineAutoConfiguration{
+
+    @Autowired
+    public DataSource dataSource;
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(dataSource);
+    }
+
+    @Bean
+    public SpringProcessEngineConfiguration springProcessEngineConfiguration() {
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+        configuration.setDataSource(dataSource);
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        configuration.setJobExecutorActivate(true);
+        configuration.setTransactionManager(transactionManager());
+        return configuration;
+    }
+
+}
